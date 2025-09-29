6ac33ac686c6ddc47d4fb4e76e54d174728f8076
Proclus Harmund <procluha@ev-h1.kzn.21-school.ru>
2025-09-29 21:12:29 +0300

    add comms

--- 05.xx/ex05_03.c
+++ 05.xx/ex05_03.c
@@ -4,22 +4,22 @@
 int main() {
     int *array = malloc(sizeof(int) * 2);
     int counter = 0;
-    if (scanf("%d", &array[0]) != 1) return printf("n/a");
+    if (scanf("%d", &array[0]) != 1) return printf("n/a");  //ввод размера массива
     while (array[counter] != -1) {
         counter++;
         array = realloc(array, sizeof(int) * (counter + 1));
-        if (scanf("%d", &array[counter]) != 1) return printf("n/a");
+        if (scanf("%d", &array[counter]) != 1) return printf("n/a");        //ввод массива и расширение памяти для каждого элемента
     }
-    int fn = 0, fk = counter - 1;
+    int fn = 0, fk = counter - 1;   //инициализация двух переменных, которые будут учавствовать в выводе
     for (int i = 0; i < counter; i++) {
-        if (i % 2 != 0) {
+        if (i % 2 != 0) {       //идем по массиву, если индекс, на котором мы находимся НЕ четный - выводим число с конца
             printf("%d", array[fk]);
             fk--;
         } else {
-            printf("%d", array[fn]);
+            printf("%d", array[fn]);        //иначе - выводим число
             fn++;
         }
-        if (i + 1 != counter) printf(" ");
+        if (i + 1 != counter) printf(" ");      //печатаем пробел, если элемент не последний
     }
     free(array);
 }
--- 05.xx/ex05_05.c
+++ 05.xx/ex05_05.c
@@ -4,13 +4,13 @@
 int main() {
     int *array = malloc(sizeof(int) * 2);
     int counter = 0;
-    if (scanf("%d", &array[0]) != 1) return printf("n/a");
+    if (scanf("%d", &array[0]) != 1) return printf("n/a");      //вводим первый элемент массива
     while (array[counter] != -1) {
         counter++;
         array = realloc(array, sizeof(int) * (counter + 1));
-        if (scanf("%d", &array[counter]) != 1) return printf("n/a");
+        if (scanf("%d", &array[counter]) != 1) return printf("n/a");        //вводим последующие элементы, расширяя память
     }
-    for (int i = counter - 1; i >= 0; i--) {
+    for (int i = counter - 1; i >= 0; i--) {        //выводим элементы массива с конца
         printf("%d", array[i]);
         if (i != 0) printf(" ");
     }
