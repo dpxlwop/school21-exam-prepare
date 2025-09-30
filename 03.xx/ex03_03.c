f479bbddbe61fc983d39aad6db846d2bb59eb7d3
Proclus Harmund <procluha@ev-h1.kzn.21-school.ru>
2025-09-30 17:05:09 +0300

    .

--- 03.xx/ex03_03.c
+++ 03.xx/ex03_03.c
@@ -5,6 +5,9 @@ int main() {
     if (scanf("%d", &number) != 1) return printf("n/a");  // ввод числа с проверкой
     new_number = (number % 10 * 100) + (number / 10 % 10 * 10) + (number / 100);
     // перестановка чисел (десятки в сотни) + (число в середине оставляем) + (сотни в десятки)
-    printf("%d", new_number);
+    if (number < 0)
+        printf("%04d", new_number);
+    else
+        printf("%03d", new_number);
     return 0;
 }
